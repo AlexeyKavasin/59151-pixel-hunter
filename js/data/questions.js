77efadd1de5594a345521109732581cda64e31c1
Alexey Kavasin (work) <akavasin@gmail.com>
2018-04-24 18:17:52 +0400

    подгрузка картинок и прочих данных с сервера

--- js/data/questions.js
+++ js/data/questions.js
@@ -1,5 +1,61 @@
 import {QUESTION_TYPES, ANSWER_TYPES} from './constants';
 
+const questions = [];
+
+const questionsData = window.fetch(`https://es.dump.academy/pixel-hunter/questions`);
+
+const adjustQuestionType = (type) => {
+  let replacedType = ``;
+  switch (type) {
+    case `two-of-two`:
+      replacedType = QUESTION_TYPES.chooseType;
+      break;
+    case `one-of-three`:
+      replacedType = QUESTION_TYPES.findPic;
+      break;
+    case `tinder-like`:
+      replacedType = QUESTION_TYPES.photoOrPic;
+      break;
+  }
+  return replacedType;
+};
+
+const createQuestionObj = (typeOfQuestion, urls, width, height, imageTypes) => {
+  questions.push({
+    type: typeOfQuestion,
+    images: urls,
+    correctAnswer: imageTypes
+  });
+};
+
+questionsData.
+    then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(`Неизвестный статус: ${response.status} ${response.statusText}`);
+    }).
+    then((data) => data.map((question) => {
+      const type = adjustQuestionType(question.type);
+      const width = [...new Set(question.answers.map((answer) => {
+        return answer.image.width;
+      }))].join(``);
+      const height = [...new Set(question.answers.map((answer) => {
+        return answer.image.height;
+      }))].join(``);
+      const urls = question.answers.map((answer) => {
+        return answer.image.url;
+      });
+      const imageTypes = question.answers.map((answer) => {
+        return answer.type;
+      });
+      // console.log(type, urls, width, height, imageTypes);
+      createQuestionObj(type, urls, width, height, imageTypes);
+    })).
+    catch((err) => console.error(err));
+
+console.dir(questions);
+
 export default [
   {
     type: QUESTION_TYPES.chooseType,
