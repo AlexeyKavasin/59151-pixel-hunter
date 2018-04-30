bd931e725a32d041ff39440c2e011ef4809c2bd4
akavasin <azamatdj@gmail.com>
2018-04-30 14:14:01 +0400

    Б7 в тестах

--- js/data/stats-count.test.js
+++ js/data/stats-count.test.js
@@ -5,69 +5,69 @@ import {Points, AnswerValues} from './constants';
 describe(`calculateStats function testing`, () => {
   it(`should be false in totalResult if answers are fewer than levels`, () => {
     let answers = [`correct`, `correct`, `wrong`, `slow`, `correct`, `wrong`, `wrong`, `slow`];
-    let state = {
-      userName: `qwerty`,
-      level: 0,
-      lives: 3,
-      timer: null
+    let State = {
+      USERNAME: `qwerty`,
+      LEVEL: 0,
+      LIVES: 3,
+      TIMER: null
     };
-    assert.equal(calculateStats(state, answers).totalResult.success, false);
+    assert.equal(calculateStats(State, answers).totalResult.success, false);
   });
   it(`should be true in totalResult if all levels passed and lives remain`, () => {
     let answers = [`correct`, `correct`, `correct`, `slow`, `correct`, `correct`, `correct`, `slow`, `fast`, `fast`];
-    let state = {
-      userName: `qwerty`,
-      level: 0,
-      lives: 3,
-      timer: null
+    let State = {
+      USERNAME: `qwerty`,
+      LEVEL: 0,
+      LIVES: 3,
+      TIMER: null
     };
-    assert.equal(calculateStats(state, answers).totalResult.success, true);
+    assert.equal(calculateStats(State, answers).totalResult.success, true);
   });
   it(`should count lives bonus correctly`, () => {
     let answers = [`correct`, `correct`, `correct`, `fast`, `correct`, `correct`, `correct`, `correct`, `slow`, `slow`];
-    let state = {
-      userName: `qwerty`,
-      level: 0,
-      lives: 3,
-      timer: null
+    let State = {
+      USERNAME: `qwerty`,
+      LEVEL: 0,
+      LIVES: 3,
+      TIMER: null
     };
-    assert.equal(calculateStats(state, answers).bonuses[2].total, Points.LIVES * state.LIVES);
+    assert.equal(calculateStats(State, answers).bonuses[2].total, Points.LIVES * State.LIVES);
   });
   it(`should count fast bonus correctly`, () => {
     let answers = [`fast`, `fast`, `correct`, `fast`, `correct`, `correct`, `correct`, `correct`, `slow`, `slow`];
     let fastAnswers = 3;
-    let state = {
-      userName: `qwerty`,
-      level: 0,
-      lives: 3,
-      timer: null
+    let State = {
+      USERNAME: `qwerty`,
+      LEVEL: 0,
+      LIVES: 3,
+      TIMER: null
     };
-    assert.equal(calculateStats(state, answers).bonuses[0].total, Points[AnswerValues.FAST] * fastAnswers);
+    assert.equal(calculateStats(State, answers).bonuses[0].total, Points[AnswerValues.FAST] * fastAnswers);
   });
   it(`should count slow bonus correctly`, () => {
     let answers = [`fast`, `fast`, `correct`, `fast`, `correct`, `correct`, `correct`, `correct`, `slow`, `slow`];
     let slowAnswers = 2;
-    let state = {
-      userName: `qwerty`,
-      level: 0,
-      lives: 3,
-      timer: null
+    let State = {
+      USERNAME: `qwerty`,
+      LEVEL: 0,
+      LIVES: 3,
+      TIMER: null
     };
-    assert.equal(calculateStats(state, answers).bonuses[1].total, Points[AnswerValues.SLOW] * slowAnswers);
+    assert.equal(calculateStats(State, answers).bonuses[1].total, Points[AnswerValues.SLOW] * slowAnswers);
   });
   it(`should count score correctly`, () => {
     let answers = [`correct`, `correct`, `correct`, `correct`, `correct`, `correct`, `wrong`, `fast`, `fast`, `slow`];
-    let state = {
-      userName: `qwerty`,
-      level: 0,
-      lives: 2,
-      timer: null
+    let State = {
+      USERNAME: `qwerty`,
+      LEVEL: 0,
+      LIVES: 2,
+      TIMER: null
     };
     let totalCorrect = answers.filter((answer) => answer !== AnswerValues.WRONG).length * Points[AnswerValues.CORRECT];
     let totalSlow = answers.filter((answer) => answer === AnswerValues.SLOW).length * Points[AnswerValues.SLOW];
     let totalFast = answers.filter((answer) => answer === AnswerValues.FAST).length * Points[AnswerValues.FAST];
-    let totalLives = state.LIVES * Points.LIVES;
+    let totalLives = State.LIVES * Points.LIVES;
     let score = totalCorrect + totalSlow + totalFast + totalLives;
-    assert.equal(calculateStats(state, answers).totalResult.score, score);
+    assert.equal(calculateStats(State, answers).totalResult.score, score);
   });
 });
